182bb8d951e5bf115f2fd53ca649ba7c062f5806
BusyginArtem <artembusygin87@gmail.com>
2025-11-16 11:45:06 +0100

    fix sections overlapping;

--- feature/projects/index.tsx
+++ feature/projects/index.tsx
@@ -2,7 +2,7 @@ import StackedCards from "./stacked-cards";
 
 export default function Projects() {
   return (
-    <div id="projects" className="py-20 md:py-32 transition-colors">
+    <div id="projects" className="mt-20 md:mt-32 transition-colors">
       <StackedCards />
     </div>
   );
