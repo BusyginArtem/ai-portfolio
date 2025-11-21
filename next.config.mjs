233014311569dd5364d131c0714cea6e413bc5e0
BusyginArtem <artembusygin87@gmail.com>
2025-11-21 13:17:38 +0100

    change image provider;

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pdf-parse", "mammoth"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "", // Leave empty for default ports (80 for http, 443 for https)
+        pathname: "/dtrl8p5mc/image/upload/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
